d7b4ab3fce67f5bbbd9999b03e7e2cc8f4ae9b32
realecto <ectos69@gmail.com>
2024-02-25 02:18:26 +1000

    Minor adjustments

--- pages/lib/firebase.js
+++ pages/lib/firebase.js
@@ -10,17 +10,8 @@ import { collection, getDocs, getFirestore, limit, query, where } from "firebase
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBM8DEtJnI5IEfTGx3KasxifCNA8xBJV1g",
-  authDomain: "henosis-999a6.firebaseapp.com",
-  databaseURL: "https://henosis-999a6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "henosis-999a6",
-  storageBucket: "henosis-999a6.appspot.com",
-  messagingSenderId: "1075807800890",
-  appId: "1:1075807800890:web:6f07f47410aa5777576f03",
-  measurementId: "G-H239R0F4WX"
-};
 
+// PASTE FIREBASE CONFIG HERE
 
 // Initialize Firebase
 if (!getApps.length){
